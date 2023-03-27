4c8b4ce1734d4a0037e6141f347599e775de4872
thekamalkashyap <kamal1729.be22@chitkara.edu.in>
2023-03-27 11:02:38 +0530

    lot more changes

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -3,17 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html className="dark">
-
       <Head />
-
       <body className=" dark:bg-gray-800 dark:text-gray-100">
-
         <Main />
-
         <NextScript />
-
       </body>
-
     </Html>
   );
 }
